01519dd1d98da0ecbed3df8889e49387a3aec055
Jin Wang <2612671w@student.gla.ac.uk>
2022-07-05 18:00:15 +0100

    vae new

--- .idea/load_VAE.py
+++ .idea/load_VAE.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
@@ -10,15 +12,16 @@ IMG_SIZE = 224
 MAX_SEQ_LENGTH = 40
 EPOCHES = 50
 BATCH_SIZE = 128
-latent_dim = 2
+latent_dim = 20
 sample_path = "./images/"
+random_seed = random.randint(1,1000000)
 
 pd_reader = pd.read_csv("./videos/video_list.csv",header=None)
 
 samples = tf.keras.preprocessing.image_dataset_from_directory(
     sample_path,
     image_size=(224,224),
-    seed=13,
+    seed=random_seed,
     subset="training",
     validation_split = 0.2,
     batch_size=BATCH_SIZE,
@@ -26,7 +29,7 @@ samples = tf.keras.preprocessing.image_dataset_from_directory(
 )
 
 AUTOTUNE=tf.data.AUTOTUNE
-samples = samples.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+samples = samples.cache("vae_train_cache").shuffle(1000).prefetch(buffer_size=AUTOTUNE)
 normalization_layer = layers.Rescaling(1./255)
 normalized_samples = samples.map(lambda x : normalization_layer(x))
 
@@ -54,14 +57,13 @@ class Sampling(layers.Layer):
 
 
 def build_encoder(latent_dim):
-    latent_dim = 2
+    latent_dim = 20
     encoder_inputs = keras.Input(shape=(IMG_SIZE, IMG_SIZE, 3))
-    x = layers.Conv2D(8, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
-    x = layers.Conv2D(16, 3, activation="relu", strides=2, padding="same")(x)
-    x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(x)
+    x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(encoder_inputs)
+    x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
     x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
     x = layers.Flatten()(x)
-    x = layers.Dense(16, activation="relu")(x)
+    x = layers.Dense(latent_dim+latent_dim)(x)
     z_mean = layers.Dense(latent_dim, name="z_mean")(x)
     z_log_var = layers.Dense(latent_dim, name="z_log_var")(x)
     z = Sampling()([z_mean, z_log_var])
@@ -69,15 +71,14 @@ def build_encoder(latent_dim):
     return encoder
 
 def build_decoder(latent_dim):
-    latent_dim = 2
+    latent_dim = 20
     latent_inputs = keras.Input(shape=(latent_dim,))
-    x = layers.Dense(14 * 14 * 64, activation="relu")(latent_inputs)
-    x = layers.Reshape((14, 14, 64))(x)
+    x = layers.Dense(28 * 28 * 64, activation="relu")(latent_inputs)
+    x = layers.Reshape((28, 28, 64))(x)
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
     x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(x)
     x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
-    x = layers.Conv2DTranspose(16, 3, activation="relu", strides=2, padding="same")(x)
-    x = layers.Conv2DTranspose(8, 3, activation="relu", strides=2, padding="same")(x)
-    decoder_outputs = layers.Conv2DTranspose(3, 3, activation="sigmoid", padding="same")(x)
+    decoder_outputs = layers.Conv2DTranspose(3, 3, strides=1, padding="same")(x)
     decoder = keras.Model(latent_inputs, decoder_outputs, name="decoder")
     return decoder
 
@@ -91,6 +92,7 @@ class VAE(Model):
             name="reconstruction_loss"
         )
         self.kl_loss_tracker = keras.metrics.Mean(name="kl_loss")
+        self.acc_tracker = tf.keras.metrics.BinaryAccuracy()
 
     def call(self,inputs):
         z_mean, log_var, z = self.encoder(inputs)
@@ -103,6 +105,7 @@ class VAE(Model):
             self.total_loss_tracker,
             self.reconstruction_loss_tracker,
             self.kl_loss_tracker,
+            self.acc_tracker,
         ]
 
     def train_step(self, data):
@@ -117,6 +120,7 @@ class VAE(Model):
             kl_loss = -0.5 * (1 + z_log_var - tf.square(z_mean) - tf.exp(z_log_var))
             kl_loss = tf.reduce_mean(tf.reduce_sum(kl_loss, axis=1))
             total_loss = reconstruction_loss + kl_loss
+        self.acc_tracker.update_state(data, reconstruction)
         grads = tape.gradient(total_loss, self.trainable_weights)
         self.optimizer.apply_gradients(zip(grads, self.trainable_weights))
         self.total_loss_tracker.update_state(total_loss)
@@ -126,16 +130,19 @@ class VAE(Model):
             "loss": self.total_loss_tracker.result(),
             "reconstruction_loss": self.reconstruction_loss_tracker.result(),
             "kl_loss": self.kl_loss_tracker.result(),
+            "accuracy": self.acc_tracker.result(),
         }
 
 
-# encoder = build_encoder(latent_dim)
-# decoder = build_decoder(latent_dim)
-# encoder.summary()
-# decoder.summary()
+encoder = build_encoder(latent_dim)
+decoder = build_decoder(latent_dim)
 
-encoder = keras.models.load_model('vae_encoder')
-decoder = keras.models.load_model('vae_decoder')
+
+# encoder = keras.models.load_model('vae_encoder')
+# decoder = keras.models.load_model('vae_decoder')
+
+encoder.summary()
+decoder.summary()
 
 def get_model(encoder,decoder):
     vae = VAE(encoder,decoder)
